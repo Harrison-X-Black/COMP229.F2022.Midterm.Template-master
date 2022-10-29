8866596770497542b2df3d49192fede64f9aa77a
Harrison-X-Black <115744146+Harrison-X-Black@users.noreply.github.com>
2022-10-29 13:45:33 -0400

    restart

--- routes/todo.js
+++ routes/todo.js
@@ -9,12 +9,7 @@ function requireAuth(req, res, next)
     // check if the user is logged in
     
     // ADD YOUR CODE HERE        
-    if(!req.isAuthenticated())
-    {
-        req.session.url = req.originalUrl;
-        return res.redirect('/users/signin');
-    }
-    next();
+
 }
 
 /* GET list of items */
